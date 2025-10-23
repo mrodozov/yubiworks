c00e07d0d59f788041487ebb89ce70668ad17a88
mrodozov <3791215+mrodozov@users.noreply.github.com>
2025-10-23 22:45:36 +0300

    remove redundant lines its wasteful

--- pivcli_go/list_keys/main.go
+++ pivcli_go/list_keys/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/go-piv/piv-go/piv"
 )
 
-
 func listKeys() {
 	cardNames, err := piv.Cards()
 	if err != nil {
@@ -24,7 +23,6 @@ func listKeys() {
 	}
 }
 
-
 func openFirstKey() {
 	// Find all cards
 	cardNames, err := piv.Cards()
@@ -35,24 +33,19 @@ func openFirstKey() {
 	if len(cardNames) == 0 {
 		log.Fatalln("No PIV cards found. Please insert a YubiKey or SoloKey.")
 	}
-
 	// Open the first card found
 	fmt.Printf("Attempting to open card: %s\n", cardNames[0])
-
 	tx, err := piv.Open(cardNames[0])
 	if err != nil {
 		log.Fatalf("Failed to open card: %v", err)
 	}
 	// This ensures the card connection is closed when the function finishes
 	defer tx.Close()
-
 	fmt.Println("Successfully opened the card!")
-
 	// Now that we have the 'tx' object, we can interact with the key.
 }
 
 func main() {
 	//openFirstKey()
-
 	listKeys()
 }
